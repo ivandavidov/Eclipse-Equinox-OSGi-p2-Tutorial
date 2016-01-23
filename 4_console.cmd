7efb85ed3ecd94417bd847e364ecf1374ae65f5b
Ivan Davidov <davidov.i@gmail.com>
2016-01-23 11:47:23 +0200

    OSGi console is now started via the launcher.

--- 4_console.cmd
+++ 4_console.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-set ORG_ECLIPSE_OSGI_VERSION=3.10.101.v20150820-1432
+set ORG_ECLIPSE_EQUINOX_LAUNCHER_VERSION=1.3.100.v20150511-1540
 
 if not exist configuration mkdir configuration
 if not exist configuration\config.ini copy /y configuration_original\config.ini configuration
 
-java -Declipse.ignoreApp=true -Dosgi.install.area=equinox_repo -Dosgi.configuration.area=configuration -jar equinox_repo\plugins\org.eclipse.osgi_%ORG_ECLIPSE_OSGI_VERSION%.jar -consoleLog -console
+java -Declipse.ignoreApp=true -Dosgi.install.area=equinox_repo -Dosgi.configuration.area=configuration -jar equinox_repo\plugins\org.eclipse.equinox.launcher_%ORG_ECLIPSE_EQUINOX_LAUNCHER_VERSION%.jar -consoleLog -console -noExit
